7b6fc6d6b9ecbec9634d9b55036c1fc38d8a3aab
Bernhard Behrendt <bernhard.bezdek@googlemail.com>
2018-03-28 23:48:37 +0200

    Dependency updates

--- src/lib/classes/options.spec.ts
+++ src/lib/classes/options.spec.ts
@@ -15,7 +15,7 @@ describe('Options', () => {
 
         let DEFAULT_OPTIONS = options.getAll(),
             EXPECTED_OPTIONS = {
-                agent: 'FetchBot-1.4.1',
+                agent: 'FetchBot-1.4.2',
                 attached: false,
                 trust: false,
                 width: 1024,
