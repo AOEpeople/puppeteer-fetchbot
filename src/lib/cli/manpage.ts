a13cc8af66ce66a119c1be9af964e7e7a78dd2b9
Bernhard Behrendt <bernhard.bezdek@googlemail.com>
2018-05-04 00:00:56 +0200

    npm readme render issue -> 1.4.11

--- src/lib/cli/manpage.ts
+++ src/lib/cli/manpage.ts
@@ -9,7 +9,7 @@ export function manPage() {
             raw: true
         },
         {
-            header: 'FetchBot (1.4.10)',
+            header: 'FetchBot (1.4.11)',
             content: 'Library and shell-command that provides a simple JSON-API to perform human like interactions and data extractions on any website. Built on top of puppeteer'
         },
         {
@@ -25,7 +25,7 @@ export function manPage() {
                 {
                     name: 'agent',
                     typeLabel: '{underline String}',
-                    description: 'The user agent string (default FetchBot-1.4.10)'
+                    description: 'The user agent string (default FetchBot-1.4.11)'
                 },
 
                 {
